8b7ca5d99fd7a669b99903b163b69992e39d18bc
Anurag Singh <anurags228@gmail.com>
2019-02-26 01:19:00 +0530

    new skill addedtry

--- skills/anurag.js
+++ skills/anurag.js
@@ -3,7 +3,7 @@
 //
 module.exports = function (controller) {
 
-    controller.hears(['hi', 'hello', 'howdy', 'hey', 'aloha', 'hola', 'bonjour', 'oi'], ['message_received'], function (bot, message) {
+    controller.hears([/^hi$/], ['message_received'], function (bot, message) {
 
         // do something to respond to message
         bot.reply(message, 'Oh hai!');
