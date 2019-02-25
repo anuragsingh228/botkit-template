134ffa0ae0cae78047ef7b7c7976332f06da6868
Anurag Singh <anurags228@gmail.com>
2019-02-26 01:59:47 +0530

    new skill added

--- skills/z-fallback.js
+++ skills/z-fallback.js
@@ -4,9 +4,9 @@
 module.exports = function (controller) {
 
     controller.hears([".*"], 'direct_message,direct_mention', function (bot, message) {
-        var mardown = "Sorry, I did not understand.<br/>Try "
+        var mardown = "Sorry hello, I did not understand.<br/>Try "
             + bot.appendMention(message, "help");
-            
+
         bot.reply(message, mardown);
     });
 }
